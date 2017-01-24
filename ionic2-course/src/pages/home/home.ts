c2bcbe29c297764e3ea175483dca1b4c9e95a1da
candreim <andrei@morera.me>
2017-01-24 17:30:25 -0600

    Completed Section [Mastering the Basics]

--- ionic2-course/src/pages/home/home.ts
+++ ionic2-course/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+//import { NavController } from 'ionic-angular';
 import {UsersPage} from "../users/users";
 
 @Component({
