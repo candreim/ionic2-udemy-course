cac3e175e9746d64e5f9a6c41ab7fa8e3afab69f
Andrei Morera <andrei@morera.me>
2017-02-20 00:16:35 -0600

    Created required pages and models

--- awesome-places/src/app/app.module.ts
+++ awesome-places/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {PlacePage} from "../pages/place/place";
+import {AddPlacePage} from "../pages/add-place/add-place";
+import {SetLocationPage} from "../pages/set-location/set-location";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PlacePage,
+    AddPlacePage,
+    SetLocationPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +20,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PlacePage,
+    AddPlacePage,
+    SetLocationPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
